fbe799d9a574505a3d24668275e30f39058c295c
Octyabr <100402230+Octyabr@users.noreply.github.com>
2022-05-27 02:54:30 +0300

    Add files via upload

---
+++ 1pr.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <time.h>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	cout << "Ââåäèòå ðàçìåð ìàññèâà: ";
+	int n;
+	cin >> n;
+	clock_t start, end;
+	start = clock();
+	int* a = new int[n];
+	for (int i = 0; i < n; i++)
+	{
+		a[i] = rand() % 100;
+		cout << a[i] << "  ";
+	}
+	cout << "\n";
+	int buff = 0;
+	int i, j;
+	for (i = 1; i < n; i++)
+	{
+		buff = a[i];
+		for (j = i - 1; j >= 0 && a[j] > buff; j--)
+			a[j + 1] = a[j];
+
+		a[j + 1] = buff;
+		cout << "ß ñïèñàë 1000-7 õåõå" << endl;
+	}
+	for (int i = 0; i < n; i++) 
+		cout << a[i] << "  ";
+	cout << endl;
+	end = clock();
+	printf("Âðåìÿ âûïîëíåíèå ïðîãðàììû %.4f(s)\n", ((double)end - start) / ((double)CLOCKS_PER_SEC));
+
+}
